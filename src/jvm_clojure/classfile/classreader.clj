78e6ae45e4de318bdc4ccdfa8da5a92b96abe3bb
胡宇靖 <hyj4work@outlook.com>
2025-01-17 14:38:55 +0800

    classreader sideEffects.

--- src/jvm_clojure/classfile/classreader.clj
+++ src/jvm_clojure/classfile/classreader.clj
@@ -1,60 +1,81 @@
 (ns jvm-clojure.classfile.classreader
   (:import (java.nio ByteBuffer ByteOrder)))
 
-(defrecord ClassReader [data]
+(defrecord ClassReader [data position]
 
   )
-(defn newClassReader [class-data] (new ClassReader class-data))
+(defn newClassReader [class-data] (new ClassReader class-data (ref {:value 0})))
+
+(defn inc-position [class-reader delta]
+  (dosync
+    (let [position (->> class-reader :position)
+          old-value (->> @position :value)
+          new-value (+ delta old-value)]
+      (dosync (ref-set position {:value new-value})))))
+
+(defn read-from-position
+  "docstring"
+  [class-reader]
+  (drop (->> class-reader :position deref :value) (:data class-reader))
+  )
 
 (defn readUint8 [class-reader]
   (let [
-        val (ByteBuffer/wrap (byte-array (take 1 (.data class-reader))))
-        the_rest (->> (drop 1 (.data class-reader)))
+        data (read-from-position class-reader)
+        val (ByteBuffer/wrap (byte-array (take 1 data)))
         ]
-    {:value (.get (.order val ByteOrder/BIG_ENDIAN)), :ref (new ClassReader the_rest)}
+    (inc-position class-reader 1)
+    {:value (.get (.order val ByteOrder/BIG_ENDIAN))}
     )
   )
+
 (defn readUint16 [class-reader]
   (let [
-        val (ByteBuffer/wrap (byte-array (take 2 (.data class-reader))))
-        the_rest (->> (drop 2 (.data class-reader)))
+        data (read-from-position class-reader)
+        val (ByteBuffer/wrap (byte-array (take 2 data)))
         ]
-    {:value (.getShort (.order val ByteOrder/BIG_ENDIAN)), :ref (new ClassReader the_rest)}
+    (inc-position class-reader 2)
+    {:value (.getShort (.order val ByteOrder/BIG_ENDIAN))}
     )
   )
+
 (defn readUint32 [class-reader]
   (let [
-        val (ByteBuffer/wrap (byte-array (take 4 (.data class-reader))))
-        the_rest (->> (drop 4 (.data class-reader)))
+        data (read-from-position class-reader)
+        val (ByteBuffer/wrap (byte-array (take 4 data)))
         ]
-    {:value (.getInt (.order val ByteOrder/BIG_ENDIAN)), :ref (new ClassReader the_rest)}
+    (inc-position class-reader 4)
+    {:value (.getInt (.order val ByteOrder/BIG_ENDIAN))}
     )
   )
 (defn readUint64 [class-reader]
   (let [
-        val (ByteBuffer/wrap (byte-array (take 8 (.data class-reader))))
-        the_rest (->> (drop 8 (.data class-reader)))
+        data (read-from-position class-reader)
+        val (ByteBuffer/wrap (byte-array (take 8 data)))
         ]
-    {:value (.getLong (.order val ByteOrder/BIG_ENDIAN)), :ref (new ClassReader the_rest)}
+    (inc-position class-reader 8)
+    {:value (.getLong (.order val ByteOrder/BIG_ENDIAN))}
     )
   )
 ; read short table, the length is the first 2 bytes
 (defn readUint16s [class-reader]
   (let [
+        data (read-from-position class-reader)
         length (->> class-reader readUint16 :value)
-        data-area (drop 2 (.data class-reader))
-        groups (->> data-area (partition 2) (take length))
+        groups (->> (drop 2 data) (partition 2) (take length))
         ]
+    (inc-position class-reader (+ 2 (* 2 length)))
     {:value (map #(.getShort
                     (.order (ByteBuffer/wrap (byte-array %)) ByteOrder/BIG_ENDIAN)) groups),
-     :ref   (new ClassReader
-                 (drop (* 2 length) data-area))}
+     }
     )
   )
 (defn readBytes [class-reader length]
-  (let [value (take length (.data class-reader))
-        the_rest (drop length (.data class-reader))
+  (let [
+        data (read-from-position class-reader)
+        value (take length data)
         ]
-    {:value value, :ref (new ClassReader the_rest)}
+    (inc-position class-reader length)
+    {:value value}
     )
   )
--- test/jvm_clojure/classfile/classreader_test.clj
+++ test/jvm_clojure/classfile/classreader_test.clj
@@ -5,19 +5,28 @@
   )
 
 (def data (byte-array [(byte 0x00) (byte 0x01) (byte 0x02) (byte 0x03) (byte 0x04) (byte 0x05) (byte 0x06) (byte 0x07)]))
-(defn cr [] (newClassReader data))
 (def data2 (byte-array [(byte 0x00) (byte 0x03) (byte 0x01) (byte 0x02) (byte 0x03) (byte 0x04) (byte 0x05) (byte 0x06) (byte 0x07)]))
-(defn cr2 [] (newClassReader data2))
 (deftest read-classfile
   (testing "read"
-    (is (= 0 (:value (readUint8 (cr)))))
-    (is (= (rest data) (:data (:ref (readUint8 (cr))))))
-    (is (= 1 (:value (readUint16 (cr)))))
-    (is (= 0x00010203 (:value (readUint32 (cr)))))
-    (is (= 0x0001020304050607 (:value (readUint64 (cr)))))
+    (is (= 0 (:value (readUint8 (newClassReader data)))))
+    (is (= 1 (:value (readUint16 (newClassReader data)))))
+    (is (= 0x00010203 (:value (readUint32 (newClassReader data)))))
+    (is (= 0x0001020304050607 (:value (readUint64 (newClassReader data)))))
     (is (= '((1 2) (3 4) (5 6) (7 8)) (partition 2 '(1 2 3 4 5 6 7 8))))
-    (is (= '(0x0102 0x0304 0x0506) (:value (readUint16s (cr2)))))
-    (is (= '(0x07) (:data (:ref (readUint16s (cr2))))))
-    (is (= '(0x00 0x01 0x02 0x03) (:value (readBytes (cr) 4))))
+    (is (= '(0x00 0x01 0x02 0x03) (:value (readBytes (newClassReader data) 4)))))
+  )
+(deftest read-classfile2
+  (testing "read"
+    (is (= '(0x0102 0x0304 0x0506) (:value (readUint16s (newClassReader data2)))))
     )
   )
+
+(deftest position-classfile
+  (testing "position"
+    (is (= {:value 1} (let [obj (newClassReader data2)]
+                        (readUint8 obj)
+                        (deref (:position obj)))))
+    (is (= {:value (+ 1 2)} (let [obj (newClassReader data2)]
+                              (is (= 0x00 (:value (readUint8 obj))))
+                              (is (= 0x0301 (:value (readUint16 obj))))
+                              (deref (:position obj)))))))
