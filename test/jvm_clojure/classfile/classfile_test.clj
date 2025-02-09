a4180d1cec0bc47bf2f760b7cc71b2cf46133d99
huyujing <hyj1127176797@hotmail.com>
2025-02-09 21:49:51 +0800

    move source to classfile.

--- test/jvm_clojure/classfile/classfile_test.clj
+++ test/jvm_clojure/classfile/classfile_test.clj
@@ -1,10 +1,11 @@
 (ns jvm-clojure.classfile.classfile-test
   (:require
+    [clojure.java.io :as io]
     [clojure.spec.alpha :as s]
     [clojure.test :refer :all]
     [jvm-clojure.classfile.classreader :refer :all]
     [jvm-clojure.classfile.classfile :refer :all])
-  )
+  (:import (java.io ByteArrayOutputStream)))
 
 (def class-data (byte-array [0xca 0xfe 0xba 0xbe 0x03 0x04 0x05 0x06 0x07]))
 (def not-class-data (byte-array [0xcc 0xfe 0xba 0xbe 0x03 0x04 0x05 0x06 0x07]))
@@ -25,3 +26,27 @@
     (is (thrown? RuntimeException (->> (newClassReader (byte-array [0x00 0x01 0x00 46])) readAndCheckVersion)))
     )
   )
+
+(def classfile-path (str "resources/testclass/Main.class"))
+(defn newMainClassData []
+  (let [out (ByteArrayOutputStream.)
+        _ (io/copy (clojure.java.io/input-stream classfile-path) out)
+        ]
+    (.toByteArray out)
+    ))
+
+(deftest testClassFile
+  (testing "testClassFile"
+    (is (= 1066 (alength (newMainClassData))))
+    (let [classFile (newClassfileV2 classfile-path)]
+      (is (= 0 (:minor_version classFile)))
+      (is (= 52 (:major_version classFile)))
+      (is (= 59 (.size (:cp_info classFile))))
+      (is (= 0x0021 (:access_flag classFile)))
+      (is (= 7 (:this_class classFile)))
+      (is (= 13 (:super_class classFile)))
+      (is (= '[14] (:interfaces classFile)))
+      (is (= 2 (.size (:fields classFile))))
+      (is (= 5 (.size (:methods classFile))))
+      (is (= 1 (.size (:attributes classFile))))
+      )))
